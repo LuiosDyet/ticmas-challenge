6532f3fbc917fa5ae5df57be62ff2d832a60ef18
LuiosD <luisdartayet@gmail.com>
2022-04-05 21:47:33 -0300

    initial docker commit

--- backend/src/middleware/credentials.js
+++ backend/src/middleware/credentials.js
@@ -1,7 +1,17 @@
 const credentials = (req, res, next) => {
-    if (req.headers.origin === process.env.FRONTEND_URL) {
+    if (req.headers.origin === 'http://localhost:3000') {
         res.header('Access-Control-Allow-Credentials', true);
-        res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
+        // res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header(
+            'Access-Control-Allow-Methods',
+            'OPTIONS, POST, GET, PUT, DELETE'
+        );
+        res.header('Access-Control-Allow-Headers', '*');
+        res.header(
+            'Access-Control-Allow-Headers',
+            'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With'
+        );
     }
     next();
 };
